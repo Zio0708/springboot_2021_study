1594d737e7d4da742430c6129395a0d6a2ad445d
Zio0708 <jiho9478@naver.com>
2021-01-04 01:46:16 +0900

    네이버 로그인까지 진행

--- src/main/java/com/springboot_2020/study/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/springboot_2020/study/config/auth/dto/OAuthAttributes.java
@@ -26,9 +26,23 @@ public class OAuthAttributes {
     }
     public static OAuthAttributes of(String registrationId ,String userNAmeAttributeName,
                                       Map<String, Object> attributes){
+        if("naver".equals(registrationId)){
+            return ofNaver("id",attributes);
+        }
         return ofGoogle(userNAmeAttributeName,attributes);
     }//느낌상 구글 네이버등 담기위해서 만든거 같긴한데..?왜? 잘모르겠음 솔직히
+    //윗글 쓰고 다음 내용에 네이버 관련 내용이 나온다.
+    private static OAuthAttributes ofNaver(String userNameAttributeName ,Map<String,Object> attributes){
+        Map<String,Object> response = (Map<String,Object>) attributes.get("response");
 
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }//구글이나 네이버나 비슷한 로그인 과정을 거치는데 이걸 줄이는 코드는 따로 없나?
     public static OAuthAttributes ofGoogle(String userNameAttributeName , Map<String, Object> attributes){
         return OAuthAttributes.builder()
                 .name((String) attributes.get("name"))
